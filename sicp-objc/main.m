142a0d7862b571c49528192be8b61b1e51e8462b
Song Zhou <zhousong1993@gmail.com>
2019-07-31 10:26:38 +0800

    Initial Commit

---
+++ sicp-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sicp-objc
+//
+//  Created by songzhou on 2019/7/31.
+//  Copyright © 2019 Song Zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
